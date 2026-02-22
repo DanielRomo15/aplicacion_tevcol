df301ba32d6229c2ce71dcb75530aa40ec737e05
DanielRomo15 <137075635+DanielRomo15@users.noreply.github.com>
2026-02-21 21:06:33 -0500

    Primer commit aplicacion_tevcol
    Change-Id: I000000000000000000000000000000000000000
    Change-Id: I56c5ca8beed4eba2929b9e93849b69b15d3b0739

---
+++ src/main/java/com/utc/aplicacion_tevcol/AplicacionTevcolApplication.java
@@ -0,0 +1,13 @@
+package com.utc.aplicacion_tevcol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionTevcolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionTevcolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utc/aplicacion_tevcol/AplicacionTevcolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc.aplicacion_tevcol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionTevcolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
